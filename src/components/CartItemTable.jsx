9e868b9a171f7ff2c24763f5a1d0490ef4b167d7
shahsuresh <shahsuresh75@gmail.com>
2024-05-01 13:20:37 +0545

    cart bot styling added

--- src/components/CartItemTable.jsx
+++ src/components/CartItemTable.jsx
@@ -72,6 +72,7 @@ const CartItemTable = ({ cartData }) => {
       component={Paper}
       sx={{
         width: "85%",
+        borderRadius:"10px",
         boxShadow:
           "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
       }}
@@ -132,6 +133,7 @@ const CartItemTable = ({ cartData }) => {
                     height: "200px",
                     width: "200px",
                     objectFit: "cover",
+                    borderRadius:"15px",
                   }}
                 />
               </TableCell>
