096abde0264536fa001b7168bed87a2760db23a1
shahsuresh <shahsuresh75@gmail.com>
2024-07-15 23:41:01 +0545

    redux toolkit installed, store and snackbarSlice is created for displaying error and success messages in snackbar

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import snackbarReducer from "./slices/snackbarSlice";
+
+const reduxStore = configureStore({
+  reducer: {
+    snackbar: snackbarReducer,
+  },
+});
+
+export default reduxStore;
