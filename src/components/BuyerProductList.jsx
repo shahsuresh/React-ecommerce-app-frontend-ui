8e04ffb71c77b5e819e49f47abe9983f76341ba6
shahsuresh <shahsuresh75@gmail.com>
2024-08-03 11:56:11 +0545

    code for filtering products added

--- src/components/BuyerProductList.jsx
+++ src/components/BuyerProductList.jsx
@@ -1,19 +1,55 @@
-import { Box, CircularProgress, FormControl, InputAdornment, OutlinedInput, Pagination, Stack, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  FormControl,
+  InputAdornment,
+  OutlinedInput,
+  Pagination,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
 import ProductCard from "./ProductCard";
 import { useQuery } from "@tanstack/react-query";
 import $axios from "../lib/axios/axios.instance";
 import Loader from "./Loader";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
 import ProductFilterDialog from "./ProductFilterDialog";
+import { useDispatch, useSelector } from "react-redux";
+import { openErrorSnackbar } from "../store/slices/snackbarSlice";
+import { clearFilter } from "../store/slices/productSlice";
 
 const BuyerProductList = () => {
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  const dispatch = useDispatch();
+  const { category, minPrice, maxPrice, isFilterApplied } = useSelector(
+    (state) => state.product
+  );
   const { isPending, data } = useQuery({
-    queryKey: ["get-buyer-products",currentPage , searchText],
+    queryKey: [
+      "get-buyer-products",
+      currentPage,
+      searchText,
+      category,
+      minPrice,
+      maxPrice,
+    ],
     queryFn: async () => {
-      return await $axios.post("/product/list/buyer", { page: currentPage, limit: 3 , searchText:searchText || null});
+      return await $axios.post("/product/list/buyer", {
+        page: currentPage,
+        limit: 3,
+        searchText: searchText || null,
+        category: category || null,
+        minPrice: minPrice || 0,
+        maxPrice: maxPrice || 0,
+      });
+    },
+    onError: (error) => {
+      dispatch(openErrorSnackbar(error.response.data.message));
+      console.log(error.res.message);
     },
   });
 
@@ -21,53 +57,62 @@ const BuyerProductList = () => {
   const totalPage = data?.data?.totalPage;
   if (isPending) {
     // return <CircularProgress />;
-    return <Loader/>
+    return <Loader />;
   }
   return (
     <>
-    <Stack direction="row" spacing="3" sx={{marginTop:"1.2rem"}}>
-      <ProductFilterDialog/>
-      <FormControl variant="standard">
+      <Stack direction='row' spacing='3' sx={{ marginTop: "1.2rem" }}>
+        {isFilterApplied && (
+          <Button
+            variant='contained'
+            color='error'
+            onClick={() => {
+              dispatch(clearFilter());
+            }}
+          >
+            Clear Filter
+          </Button>
+        )}
+        <ProductFilterDialog />
+        <FormControl variant='standard'>
           <OutlinedInput
-            defaultValue={searchText || ''}
+            defaultValue={searchText || ""}
             onChange={(event) => {
               setSearchText(event?.target?.value);
               setCurrentPage(1);
             }}
-            placeholder="Search products here..."
+            placeholder='Search products here...'
             startAdornment={
-              <InputAdornment position="start" sx={{ color: 'purple' }}>
-                <SearchIcon sx={{ fontSize: '2rem' }} />
+              <InputAdornment position='start' sx={{ color: "purple" }}>
+                <SearchIcon sx={{ fontSize: "2rem" }} />
               </InputAdornment>
             }
           />
         </FormControl>
-    </Stack>
+      </Stack>
 
-    <Box
-      sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "3rem",
-        margin:"1rem 0"
-       
-      }}
-    >
-      {productList.map((item) => {
-        return <ProductCard key={item._id} {...item} />;
-      })}
-    </Box>
-    <Pagination
-    page={currentPage}
-    count={totalPage}
-    color="secondary"
-    
-    onChange={(_, value) => {
-      setCurrentPage(value);
-    }}
-  />
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "3rem",
+          margin: "1rem 0",
+        }}
+      >
+        {productList?.map((item) => {
+          return <ProductCard key={item._id} {...item} />;
+        })}
+      </Box>
+      <Pagination
+        page={currentPage}
+        count={totalPage}
+        color='secondary'
+        onChange={(_, value) => {
+          setCurrentPage(value);
+        }}
+      />
     </>
   );
 };
