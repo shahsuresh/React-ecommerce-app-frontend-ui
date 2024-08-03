9af85c6c2e5f7b2601c41c9dce75667c964b976e
shahsuresh <shahsuresh75@gmail.com>
2024-08-03 11:54:36 +0545

    reducers for filter product added in ReduxStore

---
+++ src/store/slices/productSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    category: null,
+    minPrice: 0,
+    maxPrice: 0,
+    isFilterApplied: false,
+  },
+  reducers: {
+    setFilter: (state, action) => {
+      state.category = action.payload.category;
+      state.minPrice = action.payload.minPrice;
+      state.maxPrice = action.payload.maxPrice;
+      state.isFilterApplied = true;
+    },
+    clearFilter: (state, action) => {
+      state.category = "";
+      state.minPrice = 0;
+      state.maxPrice = 0;
+      state.isFilterApplied = false;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { setFilter, clearFilter } = productSlice.actions;
+
+export default productSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import snackbarReducer from "./slices/snackbarSlice";
+import productReducer from "./slices/productSlice";
 
 const reduxStore = configureStore({
   reducer: {
     snackbar: snackbarReducer,
+    product: productReducer,
   },
 });
 
