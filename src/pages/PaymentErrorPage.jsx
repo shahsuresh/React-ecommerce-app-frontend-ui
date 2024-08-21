53172c7c2ecb952999bc2a1b33c42da6bc92ad98
shahsuresh <shahsuresh75@gmail.com>
2024-08-22 00:44:53 +0545

    payment error and success page added

--- src/pages/PaymentErrorPage.jsx
+++ src/pages/PaymentErrorPage.jsx
@@ -1,15 +1,24 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PaymentErrorPage = () => {
   return (
     <div
       style={{
-        margin: "100px",
-        padding: "5rem",
+        margin: "50px",
+        padding: "2rem",
         boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
       }}
     >
+      <center>
+        {" "}
+        <img
+          src='/gif/error.gif '
+          alt='error'
+          style={{ width: "200px", height: "200px" }}
+        />
+      </center>
       <Typography variant='h4' sx={{ color: "red" }}>
         Error Processing payment
       </Typography>
@@ -17,6 +26,16 @@ const PaymentErrorPage = () => {
       <Typography variant='h5'>
         Sorry!! Your Order has not been placed
       </Typography>
+      <Link to='/cart'>
+        <Button
+          variant='contained'
+          color='error'
+          fullWidth
+          sx={{ marginTop: "3rem", padding: "1rem", fontSize: "1.2rem" }}
+        >
+          Goto Cart Page
+        </Button>
+      </Link>
     </div>
   );
 };
--- src/pages/PaymentSuccess.jsx
+++ src/pages/PaymentSuccess.jsx
@@ -1,21 +1,41 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const PaymentSuccess = () => {
+  const navigate = useNavigate();
   return (
     <div
       style={{
-        margin: "100px",
-        padding: "5rem",
+        margin: "50px",
+        padding: "2rem",
         boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
       }}
     >
+      <center>
+        <img
+          src='/gif/success.gif'
+          alt='success'
+          style={{ width: "250px", height: "250px" }}
+        />
+      </center>
       <Typography variant='h3' sx={{ color: "blue" }}>
         Payment Success.
       </Typography>
       <hr />
       <Typography variant='h4'>Thank You for your Order.</Typography>
+
+      <Link to='/order'>
+        <Button
+          variant='contained'
+          color='success'
+          fullWidth
+          sx={{ marginTop: "3rem", padding: "1rem", fontSize: "1.2rem" }}
+        >
+          Your Orders
+        </Button>
+      </Link>
     </div>
   );
 };
